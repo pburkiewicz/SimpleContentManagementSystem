aeb6c6b2c1c4345a2337e8e53fd115ccab6da151
Student <pawcio27610@wp.pl>
2020-01-16 18:16:32 +0100

    acceptance test 01

--- laravel_cms/tests/Acceptance/00_loginCept.php
+++ laravel_cms/tests/Acceptance/00_loginCept.php
@@ -1,7 +1,21 @@
 <?php
 $I = new AcceptanceTester($scenario);
-$I->wantTo('login with existing user');
+$I->wantTo('register and login');
 
-$I->amOnPage('/home');
+$I->amOnPage('/register');
 
-$I->seeCurrentUrlEquals('/login');
+$I->seeCurrentUrlEquals('/register');
+
+$I->fillField('first_name', 'john');
+$I->fillField('last_name', 'doe');
+$I->fillField('nick', 'johny');
+$I->fillField('page_name', 'john.doe');
+$I->fillField('email', 'john.doe@gmail.com');
+$I->fillField('password', '12345678');
+$I->fillField('password_confirmation', '12345678');
+
+$I->click('#register');
+
+$I->seeCurrentUrlEquals('/home');
+
+$I->see('You are logged in!', 'div.card-body');
