a1c99055eedfa848c1031c2faeffc0bff0040e20
Grzegorz Domaradzki <grzedom@onet.pl>
2020-01-17 06:17:52 +0100

    Minor error fixed - image render in index pages

--- laravel_cms/resources/views/blogs/index.blade.php
+++ laravel_cms/resources/views/blogs/index.blade.php
@@ -3,9 +3,6 @@
 @section('content')
 
     @includeIf("templates." . "default"  . ".head_display_index")
-        @if($galleries)
-            @includeIf("templates." . "default"  . ".image_display_index")
-        @endif
         @includeIf("templates." . "default"  . ".post_display_index")
     @includeIf("templates." . "default"  . ".tail_display")
 
