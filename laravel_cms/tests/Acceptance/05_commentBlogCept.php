8446d320a098fce480fd024c9bbe3c67f61ec018
Student <pawcio27610@wp.pl>
2020-01-18 15:57:46 +0100

    acceptance test 05 without delete

---
+++ laravel_cms/tests/Acceptance/05_commentBlogCept.php
@@ -0,0 +1,40 @@
+<?php
+$I = new AcceptanceTester($scenario);
+$I->wantTo('add, edit, delete comment');
+
+$I->amOnPage("/john_doe/tytul_strony/blog/1");
+
+$I->click("Add comment");
+$I->seeCurrentUrlEquals("/john_doe/tytul_strony/blog/1/comments/create");
+$I->fillField("#contents","komentarz pod postem od anonimowego uzytkownika");
+$I->click("Add comment");
+
+$I->seeInCurrentUrl("/john_doe/tytul_strony/blog/1");
+$I->click('Login');
+$I->seeCurrentUrlEquals('/login');
+$I->fillField('email', 'john.doe@gmail.com');
+$I->fillField('password', '12345678');
+$I->click('#login');
+$I->seeCurrentUrlEquals('/john_doe');
+$I->click("Manage your website");
+$I->click("tytul_strony - blog");
+$I->click("show");
+$I->seeInCurrentUrl("/john_doe/tytul_strony/blog/1");
+
+$I->click("Add comment");
+$I->seeCurrentUrlEquals("/john_doe/tytul_strony/blog/1/comments/create");
+$I->fillField("#contents","komentarz pod postem od zalogowanego uzytkownika");
+$I->click("Add comment");
+
+$I->seeInCurrentUrl("/john_doe/tytul_strony/blog/1");
+$I->see("Anonymous user");
+$I->see("komentarz pod postem od anonimowego uzytkownika");
+$I->see("johny");
+$I->see("komentarz pod postem od zalogowanego uzytkownika");
+$I->click("edit comment");
+$I->seeInCurrentUrl("/john_doe/tytul_strony/blog/1/comments/2/edit");
+$I->fillField("#contents","zmieniony komentarz");
+$I->click("Edit comment");
+$I->seeInCurrentUrl("/john_doe/tytul_strony/blog/1");
+$I->see("zmieniony komentarz");
+$I->dontSee("komentarz pod postem od zalogowanego uzytkownika");
