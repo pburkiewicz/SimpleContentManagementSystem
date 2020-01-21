725c40433b566eb05fbe8bc53fd400dee6562386
Grzegorz Domaradzki <grzedom@onet.pl>
2020-01-21 20:38:44 +0100

    Explicit placeholders removed.

--- laravel_cms/resources/views/welcome.blade.php
+++ laravel_cms/resources/views/welcome.blade.php
@@ -81,7 +81,7 @@
 
             <div class="content">
                 <div class="title m-b-md">
-                    Zajebisty CMS
+                    CMS
                 </div>
                 <!--
                 <div class="links">
